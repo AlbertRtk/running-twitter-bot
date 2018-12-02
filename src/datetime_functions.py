cb8522ef78165d801bed7645e81ffa607890fbac
Albert <ratajczakalbert@gmail.com>
2018-12-02 19:27:50 +0100

    Debugging date_of_nearest()

--- src/datetime_functions.py
+++ src/datetime_functions.py
@@ -1,15 +1,23 @@
 """ 2018 by Albert Ratajczak
 """
-from datetime import date
+from datetime import date, timedelta
 
 
 def date_of_nearest(day):
     """
     :param day: day of week as int - Monday = 1 ... Sunday = 7
-    :return: date of nearest day
+    :return: date of next nearest day
     """
+    # today_day = date(2018, 12, 3)  # for debugging
     today = date.today()
-    return today + timedelta(days=day-today.isoweekday())
+    today_day = today.isoweekday()
+    if today_day == day:
+        days = 7
+    elif today_day < day:
+        days = day - today_day
+    else:  # today_day > day
+        days= 7 + day - today_day
+    return today + timedelta(days=days)
 
 
 def date_with_dots(date_obj):
--- src/main.py
+++ src/main.py
@@ -8,18 +8,23 @@ import tweepy
 # Local imports
 from event import get_events_2
 from eventsstorage import EventsStorage
-from datetime_functions import date_with_dots
+from datetime_functions import date_with_dots, date_of_nearest
 
 # Twitter's APIs keys and tokens
 from api_keys import *
 
 
+# Setting Twitter's APIs
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
 twitter = tweepy.API(auth)
 
 
-tweet_templates = ['{}, {}, dnia {}, dystans:{}\n{}',
+# Database to store info about events
+storage = EventsStorage('events.db')
+
+
+tweet_templates = ['{}, {}, dnia {}, dystans: {}\n{}',
                    'Kto biegnie w {}? {}, {}. Dystans: {}\n{}',
                    'Kto startuje w {}? {}, {}. Dystans: {}\n{}',
                    'Forma na {} gotowa? {}, już {} start na: {}\n{}',
@@ -36,21 +41,33 @@ def create_tweet(event):
 
 
 def main():
+    # today_day = date(2018, 12, 3)  # for debugging
     today_date = date.today()
-    storage = EventsStorage('events.db')
-    todays_events = storage.read(date(2018,12,2))
 
     if today_date.isoweekday() == 7:
         new_events = get_events_2(today_date+timedelta(days=1), 7)
         storage.store(new_events)
 
-    for e in todays_events:
-        print(create_tweet(e))
-        print()
-        # storage.remove(e)
+    tweet_events = storage.read(today_date+timedelta(days=1))
 
-    twitter.update_status('Hej! To jest pierszy tweet!')
+    if today_date.isoweekday() not in (5, 6):
+        saturday_events = storage.read(date_of_nearest(day=6))
+        sunday_events = storage.read(date_of_nearest(day=7))
+        saturday_events = saturday_events[:int(len(saturday_events)/7)]
+        sunday_events = sunday_events[:int(len(sunday_events)/7)]
+        tweet_events += saturday_events + sunday_events
+
+    # while today_date == date.today():
+    #     pass
+
+    for e in tweet_events:
+        tweet = create_tweet(e)
+        print('\nTweeting: \n' + tweet)
+        # twitter.update_status(tweet)
+        # storage.remove(e)
 
 
 if __name__ == '__main__':
+    # while True:
+    #     main()
     main()
